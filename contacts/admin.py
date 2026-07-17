423c021406413fbc75c41473dcc4685acd8cfcb6
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-07-17 10:15:34 +0200

    Create models, make migrations and register models in admin panel

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact, ContactStatusChoices
+
+@admin.register(ContactStatusChoices)
+class ContactStatusChoicesAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone_number', 'city', 'status', 'created_at', 'updated_at')
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number', 'city')
+    list_filter = ('status', 'created_at', 'updated_at')
+    ordering = ('-created_at',)
